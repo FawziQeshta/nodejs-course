350a9ef0773a766465f6d8d4fefa3c4a00ea9592
FawziQeshta <45800534+FawziQeshta@users.noreply.github.com>
2026-02-12 12:03:31 +0200

    public style folder, read data and livereload

--- app.js
+++ app.js
@@ -3,15 +3,46 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 app.use(express.urlencoded({ extended: true }));
+const LoginModel = require('./models/loginSchema'); // Import the LoginModel from the loginSchema.js file
+app.set('view engine', 'ejs');
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+
+// Auto refresh pages
+const path = require('path');
+const livereload = require('livereload');
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+const connectLivereload = require('connect-livereload');
+app.use(connectLivereload());
+
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
+
 
 app.get('/', (req, res) => {
 //   res.send('<h1>First App Hello World!</h1>')
     // res.sendFile(__dirname + '/views/home.html')
-    res.sendFile('./views/home.html', { root: __dirname })
+    res.render('home.ejs', { title: 'Home Login' }) // Render the home.ejs template and pass a title variable
+    // بيتحكم في اسم الصفحة اللي هتظهر
 })
 
 app.get('/success-page', (req, res) => {
-    res.sendFile('./views/success-page.html', { root: __dirname })
+      LoginModel.find().then(
+        (data) => {
+            res.render('success-page.ejs', {userData: data.at(-1)}) // Render the success-page.ejs template and pass the retrieved login data as usersData
+            // data.at(-1) is used to get the last element of the data array, which is the most recently added login data. This allows us to display the email of the user who just logged in on the success page.
+        }
+    ).catch(
+        err => {
+            console.error('Error retrieving login data:', err);
+        }
+    ) // Retrieve all login data from the database and log it to the console
 })
 
 app.listen(port, () => {
@@ -24,7 +55,6 @@ mongoose.connect('mongodb+srv://eng_fawzi:PAtP3HKvVm8OLFJ7@cluster0.rkmonru.mong
   .catch(err => console.error(err));
 
 
-const LoginModel = require('./models/loginSchema'); // Import the LoginModel from the loginSchema.js file
 
 app.post('/login', (req, res) => {
 
